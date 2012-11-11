104cc4c1ca9231b07b64c6406b8d5f015b6eeb13
devsnd <tomwallroth@gmail.com>
2012-11-11 18:09:55 +0100

    Improved album art fetcher. Added new method, buy.com

--- cherrymusicserver/albumartfetcher.py
+++ cherrymusicserver/albumartfetcher.py
@@ -9,21 +9,26 @@ from cherrymusicserver import log
 
 class AlbumArtFetcher:
     def __init__(self,method='amazon', timeout=10):
+        self.methods = {
+            'amazon' : {
+                'url' : "http://www.amazon.com/s/ref=sr_nr_i_0?rh=k:",
+                'regex' : '<img  src="([^"]*)" class="productImage"'
+            },
+            'buy.com' : {
+                'url' : "http://www.buy.com/sr/srajax.aspx?from=2&qu=",
+                'regex' : ' class="productImageLink"><img src="([^"]*)"'
+            },
+        }
+        if not method in self.methods:
+            log.e('unknown album art fetch method: %s, using default.'%self.method)
+            method = 'amazon'
         self.method = method
         self.timeout = timeout
         
-    def fetch(self, searchterms):
-        if self.method == 'amazon':
-            searchterms = unidecode(searchterms)
-            return self.fetchAmazon(searchterms)
-        else:
-            log.e('unknown method: %s'%self.method)
-            
-    def fetchurl(self, searchterms):
-        if self.method == 'amazon':
-            return self.fetchAmazon(searchterms, urlonly=True)
-        else:
-            log.e('unknown method: %s'%self.method)
+    def fetch(self, searchterms, urlonly=False):
+        searchterms = unidecode(searchterms).lower()
+        searchterms = re.sub('[^a-z\s]','',searchterms)
+        return self.fetchAlbumArt(self.methods[self.method], searchterms, urlonly)
     
     def retrieveData(self, url):
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19 (KHTML, like Gecko) Ubuntu/12.04 Chromium/18.0.1025.168 Chrome/18.0.1025.168 Safari/535.19'
@@ -39,11 +44,11 @@ class AlbumArtFetcher:
     def retrieveWebpage(self, url):
         return codecs.decode(self.retrieveData(url)[0],'UTF-8')
 
-    def fetchAmazon(self, searchterm, urlonly=False):
+    def fetchAlbumArt(self, method, searchterm, urlonly=False):
         urlkeywords = urllib.parse.quote(searchterm)
-        url = "http://www.amazon.com/s/ref=sr_nr_i_0?rh=k:"+urlkeywords
+        url = method['url']+urlkeywords
         html = self.retrieveWebpage(url)   
-        matches = re.findall('<img  src="([^"]*)" class="productImage"',html)
+        matches = re.findall(method['regex'],html)
         if matches:
             if urlonly:
                 return matches[0]
@@ -53,3 +58,4 @@ class AlbumArtFetcher:
             if urlonly:
                 return ''
             return None,''
+       
