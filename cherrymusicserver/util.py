6951a985bd2650beaf3dfa38366a407bd7c2cb3b
devsnd <tomwallroth@gmail.com>
2012-11-17 18:06:11 +0100

    Added dependency installer.

--- cherrymusicserver/util.py
+++ cherrymusicserver/util.py
@@ -197,3 +197,43 @@ class Performance:
             duration = (time() - self.time)*1000
             log.w('|   '*(Performance.indentation-1) + '\__ %g ms' % (duration,))
             Performance.indentation -= 1
+
+class DependencyInstaller:
+    """
+    cherrypy releases: https://bitbucket.org/cherrypy/cherrypy/downloads
+    """
+    cherrypyurl = "https://bitbucket.org/cherrypy/cherrypy/get/default.tar.gz"
+    cherrypysubfolder = os.path.join('cherrypy-cherrypy-efb777a00739','cherrypy')
+    def __init__(self):
+        import urllib.request
+        import tempfile
+        import tarfile
+        import shutil
+        cherrypytempfile = os.path.join(tempfile.gettempdir(),'cherrypy.tar.gz')
+        cherrypytempdir =  os.path.join(tempfile.gettempdir(),'cherrypy')
+        log.i('Downloading cherrypy...')
+        with open(cherrypytempfile, 'wb') as f:
+            urlhandler = urllib.request.urlopen(urllib.request.Request(DependencyInstaller.cherrypyurl))
+            f.write(urlhandler.read())
+        log.i('Extracting %s ' % cherrypytempfile)
+        with tarfile.open(cherrypytempfile,'r:gz') as tarc:
+            if os.path.exists(cherrypytempdir):
+                log.w("Directory %s already exists." % cherrypytempdir)
+                if 'y' == input('Do you want to delete its contents and proceed? [y/n]'):
+                    shutil.rmtree(cherrypytempdir)
+                else:
+                    log.e("Cannot install cherrypy.")
+                    exit(1)
+            os.mkdir(cherrypytempdir)
+            tarc.extractall(cherrypytempdir)
+        cherrymusicfolder = os.path.dirname(os.path.dirname(__file__))
+        log.i('Copying cherrypy module inside cherrymusic folder (%s)...' % cherrymusicfolder)
+        moduledir = os.path.join(cherrypytempdir,DependencyInstaller.cherrypysubfolder)
+        shutil.copytree(moduledir,os.path.join(cherrymusicfolder,'cherrypy'))
+        log.i('Cleaning up temporary files...')
+        shutil.rmtree(cherrypytempdir)
+        os.remove(cherrypytempfile)
+        log.i('Successfully installed cherrymusic dependencies! You can now start cherrymusic.')
+        
+        
+        
