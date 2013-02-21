devsnd/cherrymusic

commit 066a99f974f4c253f83ed97f1f119a3f4ed243f7

whoops, no ifdef like codechanges....

cherrymusicserver/log.py
@@ -68,10 +68,7 @@ formatter_briefest = logging.Formatter(fmt='[%(asctime)s] %(message)s', datefmt=
 formatter_brief = logging.Formatter(fmt='[%(asctime)s] %(levelname)-8s: %(message)s', datefmt='%y%m%d-%H:%M')
 formatter_full = logging.Formatter(fmt=('-'*80)+ '\n%(levelname)-8s [%(asctime)s] : %(name)-20s : from line (%(lineno)d) at\n\t%(pathname)s\n\t--\n\t%(message)s\n')
 
-try:
-    handler_console = logging.StreamHandler(stream=sys.stdout)
-except TypeError: #python 2.6 doesn't support stream argument
-    handler_console = logging.StreamHandler()
+handler_console = logging.StreamHandler()
     
 handler_console.formatter = formatter_briefest
 handler_console.level = DEBUG

backport/logging/__init__.py
@@ -3,6 +3,6 @@ import sys
 if sys.version_info < (2,7):
     StreamHandler.__realinit__ = StreamHandler.__init__
     def initStreamHandler(self,stream):
-        StreamHandler.__realinit__()
+        StreamHandler.__realinit__(self)
     StreamHandler.__init__ = initStreamHandler
     

commit f79f31710f5dafff5fc009116d1d355a41c397fa

fixed error introduced in 066a99f974f4c253f83ed97f1f119a3f4ed243f7

cherrymusicserver/log.py
@@ -68,7 +68,7 @@ formatter_briefest = logging.Formatter(fmt='[%(asctime)s] %(message)s', datefmt=
 formatter_brief = logging.Formatter(fmt='[%(asctime)s] %(levelname)-8s: %(message)s', datefmt='%y%m%d-%H:%M')
 formatter_full = logging.Formatter(fmt=('-'*80)+ '\n%(levelname)-8s [%(asctime)s] : %(name)-20s : from line (%(lineno)d) at\n\t%(pathname)s\n\t--\n\t%(message)s\n')
 
-handler_console = logging.StreamHandler()
+handler_console = logging.StreamHandler(stream=sys.stdout)
     
 handler_console.formatter = formatter_briefest
 handler_console.level = DEBUG