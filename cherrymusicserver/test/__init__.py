aec79014f4deb96e130e42a258bf010e15c03893
til <tilman.boerner@gmx.net>
2013-02-21 20:26:30 +0100

    python 2.6 compatibility for cherrymusicserver.test and  runtests

---
+++ cherrymusicserver/test/__init__.py
@@ -0,0 +1,25 @@
+import os
+import pkgutil
+import re
+import unittest
+
+pattern = re.compile('test.*')
+loader = unittest.defaultTestLoader
+
+
+class All(unittest.TestSuite):
+    def __init__(self):
+        super(self.__class__, self).__init__(discover_tests())
+
+
+def discover_tests():
+    package = __package__
+    packdir = __file__.rsplit(os.path.sep, 1)[0]
+    parent = None
+    modules = (i[1] for i in pkgutil.iter_modules([packdir]))
+    testmodules = ('.'.join((package, m)) for m in modules if pattern.match(m))
+    for mname in testmodules:
+        yield loader.loadTestsFromName(mname, parent)
+
+if __name__ == '__main__':
+    unittest.main()
--- cherrymusicserver/test/__main__.py
+++ cherrymusicserver/test/__main__.py
@@ -8,15 +8,7 @@ pattern = re.compile('test.*')
 loader = unittest.defaultTestLoader
 runner = unittest.TextTestRunner()
 
-
-def discover_tests():
-    package = __package__
-    packdir = __file__.rsplit(os.path.sep, 1)[0]
-    parent = None
-    modules = (i[1] for i in pkgutil.iter_modules([packdir]))
-    testmodules = ('.'.join((package, m)) for m in modules if pattern.match(m))
-    for mname in testmodules:
-        yield loader.loadTestsFromName(mname, parent)
+from . import discover_tests
 
 class Fail(unittest.TestCase):
     def testfail(self):
