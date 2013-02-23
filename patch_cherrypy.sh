ad63f2d77f24cab61abef53a1d80bae7e1d38889
til <tilman.boerner@gmx.net>
2013-02-23 14:07:29 +0100

    forgodsake. try something else, then.

--- patch_cherrypy.sh
+++ patch_cherrypy.sh
@@ -2,20 +2,26 @@
 
 patch="$1"
 
-# if we're in a Travis CI environment
-if [ $TRAVIS_PYTHON_VERSION ]; then
-	pyversion=$TRAVIS_PYTHON_VERSION
-	cherrypy="../../lib/python${pyversion}/site-packages/cherrypy"
-fi
-
-# we're probably not in Travis
-if [ ! -d $cherrypy ]; then
-	if [ $2 ]; then
-		cherrypy="$2"
-	else
-		# try to get cherrypy location from within python
-		cherrypy=$(python -c "import os, cherrypy; print(os.path.split(cherrypy.__file__)[0])")
+if [ "$2" ]; then
+	cherrypy="$2"
+else
+	if [ $TRAVIS_PYTHON_VERSION ]; then
+		echo "trying travis environment..."
+		pyversion=$TRAVIS_PYTHON_VERSION
+		cherrypy="../../lib/python${pyversion}/site-packages/cherrypy"
+		if [ ! -d $cherrypy ]; then
+			cherrypy="/home/travis/virtualenv/python${pyversion}/lib/python${pyversion}/site-packages/cherrypy"
+		fi
 	fi
 fi
 
+if [ ! $cherrypy ]; then
+	echo "trying to get cherrypy location from within python"
+	cherrypy=$(python -c "import os, cherrypy; print(os.path.split(cherrypy.__file__)[0])")
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
+fi
+
+echo "using dir ${cherrypy}"
 patch -p1 -d "$cherrypy" < "$patch"
