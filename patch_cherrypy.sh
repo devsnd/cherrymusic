440a6a0f1bf5b88ffac3178461e12d58a0424a36
til <tilman.boerner@gmx.net>
2013-02-23 13:31:58 +0100

    fix travis patch script for when cherrypy won't import

--- patch_cherrypy.sh
+++ patch_cherrypy.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
-patch=$1
-cherrypy=$(python -c "import os, cherrypy; print(os.path.split(cherrypy.__file__)[0])")
-patch -p1 -d ${cherrypy} < ${patch}
+patch="$1"
+
+# if we're in a Travis CI environment
+if [ $TRAVIS_PYTHON_VERSION ]; then
+	pyversion=$TRAVIS_PYTHON_VERSION
+	cherrypy="../../lib/python/${pyversion}/site-packages/cherrypy"
+fi
+
+# we're probably not in Travis
+if [ ! -d $cherrypy ]; then
+	if [ $2 ]; then
+		cherrypy="$2"
+	else
+		# try to get cherrypy location from within python
+		cherrypy=$(python -c "import os, cherrypy; print(os.path.split(cherrypy.__file__)[0])")
+	fi
+fi
+
+patch -p1 -d "$cherrypy" < "$patch"
