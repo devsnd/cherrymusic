37a2d3d78992cc4da126a287a7003b5560b4515c
tilboerner <tilman.boerner@gmx.net>
2013-02-23 02:16:03 +0100

    travis_test: use bourne shell test syntax

--- test_travis_ci.sh
+++ test_travis_ci.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [[ $TRAVIS_PYTHON_VERSION = 2.6 ]]; then
+if [ $TRAVIS_PYTHON_VERSION == '2.6' ]; then
     unit2 discover -s ./cherrymusicserver/test -t .
 else
     python -m unittest discover -s ./cherrymusicserver/test -t .
