b4f9a4517e93a1117fcf2b792f872ebf35304710
til <tilman.boerner@gmx.net>
2013-02-23 01:22:03 +0100

    try to make tests run for glorious future

--- cherrymusicserver/test/__init__.py
+++ cherrymusicserver/test/__init__.py
@@ -1,5 +1,9 @@
-import unittest2 as unittest
+import sys
+
+if sys.version_info >= (2, 7):
+    import unittest
+else:
+    import unittest2 as unittest
 
 if __name__ == '__main__':
     unittest.main()
-
--- test_travis_ci.sh
+++ test_travis_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [[ $TRAVIS_PYTHON_VERSION = 2.6 ]]; then
-    unit2 discover --failfast -s ./cherrymusicserver/test -t .
+    unit2 discover -s ./cherrymusicserver/test -t .
 else
-    python -m unittest discover --failfast -s ./cherrymusicserver/test -t .
+    python -m unittest discover -s ./cherrymusicserver/test -t .
 fi
 exit $?
