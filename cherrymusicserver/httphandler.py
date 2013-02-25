e927516c3369dbb8decc3544a96c74dc8c1c5119
devsnd <tomwallroth@gmail.com>
2013-02-25 16:42:39 +0100

    Improved session dropping behavior on version switch, added ipv6 support

--- cherrymusicserver/httphandler.py
+++ cherrymusicserver/httphandler.py
@@ -167,7 +167,7 @@ class HTTPHandler(object):
         return cherrypy.session.get('username', None) or self.autoLoginEnabled()
 
     def autoLoginEnabled(self):
-        if cherrypy.request.remote.ip == '127.0.0.1' and cherry.config.server.localhost_auto_login.bool:
+        if cherrypy.request.remote.ip not in ('127.0.0.1', '::1') and cherry.config.server.localhost_auto_login.bool:
             cherrypy.session['username'] = self.userdb.getNameById(1)
             cherrypy.session['userid'] = 1
             cherrypy.session['admin'] = True
@@ -177,17 +177,17 @@ class HTTPHandler(object):
     def session_auth(self, username, password):
         try:
             user = self.userdb.auth(username, password)
-            if user.isadmin and not cherry.config.server.permit_remote_admin_login and not cherrypy.request.remote.ip == '127.0.0.1':
-                user = userdb.User.nobody()
-            cherrypy.session['username'] = user.name
-            cherrypy.session['userid'] = user.uid
-            cherrypy.session['admin'] = user.isadmin
         except (UnicodeDecodeError, ValueError) as e:
-            estr = str(e)
-            if estr.startswith("'ascii' codec can't decode byte") or estr == "unsupported pickle protocol: 3": 
-                log.w('Dropping all sessions! Try not to change between python 2 and 3, everybody has to relogin now.')
-                cherrypy.session.delete()
+            # workaround for python2/python3 jump, filed bug in cherrypy
+            # https://bitbucket.org/cherrypy/cherrypy/issue/1216/sessions-python2-3-compability-unsupported
+            log.w('Dropping all sessions! Try not to change between python 2 and 3, everybody has to relogin now.')
+            cherrypy.session.delete()
+            #retry with new session:
+            user = self.userdb.auth(username, password)
         finally:
+            if user.isadmin and not cherry.config.server.permit_remote_admin_login.bool and not cherrypy.request.remote.ip in ('127.0.0.1', '::1'):
+                log.i('Rejected remote admin login from user: %s'%user.name)
+                user = userdb.User.nobody()
             cherrypy.session['username'] = user.name
             cherrypy.session['userid'] = user.uid
             cherrypy.session['admin'] = user.isadmin
