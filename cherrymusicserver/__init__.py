922d7b9568578f1d1dfc559466564c3208e09eee
devsnd <tomwallroth@gmail.com>
2013-05-14 23:51:26 +0200

    Increased version to 0.25.1 for hotfix

--- cherrymusicserver/__init__.py
+++ cherrymusicserver/__init__.py
@@ -92,7 +92,7 @@ from cherrymusicserver import userdb
 from cherrymusicserver import useroptiondb
 import cherrymusicserver.browsersetup
 
-VERSION = "0.25.0"
+VERSION = "0.25.1"
 DESCRIPTION = "an mp3 server for your browser"
 LONG_DESCRIPTION = """CherryMusic is a music streaming
     server written in python. It's based on cherrypy and jPlayer.
