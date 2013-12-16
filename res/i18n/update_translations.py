a30557e4c8caf8696a32146296a73ab7b95888fd
til <tilman.boerner@gmx.net>
2013-12-16 22:37:52 +0100

    Relative paths for i18n update script

--- res/i18n/update_translations.py
+++ res/i18n/update_translations.py
@@ -2,8 +2,8 @@
 import subprocess
 import os
 
-currdir = os.path.dirname(os.path.abspath(__file__))
-sourcedir = os.path.join(currdir, '../../cherrymusicserver')
+currdir = os.path.dirname(__file__)
+sourcedir = os.path.normpath(os.path.join(currdir, '..', '..', 'cherrymusicserver'))
 
 print('updating pot file')
 subprocess.call('xgettext --language=Python --keyword=_ --output='+currdir+'/cherrymusic.pot --from-code=UTF-8 `find '+sourcedir+' -name "*.py"`', shell=True)
@@ -15,4 +15,4 @@ for translation in os.listdir(currdir):
         subprocess.call('msgmerge --update '+transfile+'/LC_MESSAGES/default.po '+currdir+'/cherrymusic.pot', shell=True)
         print('    compiling %s' % transfile)
         subprocess.call('msgfmt -o '+transfile+'/LC_MESSAGES/default.mo -v '+transfile+'/LC_MESSAGES/default.po', shell=True)
-        
+
