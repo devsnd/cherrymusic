0642ef8b5011b1bcab2b7ea4c23ddc9b9c72479e
Pando85 <pando855@gmail.com>
2016-01-11 01:54:30 +0100

    Remove workaround try to up -d before run test

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker-compose build
 docker-compose -f test.yml build
+docker-compose -f test.yml up -d
 docker-compose -f test.yml run --rm web_test /usr/src/test.sh && \
 docker-compose -f test.yml stop
--- web-test/test.sh
+++ web-test/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 coverage run manage.py test cherrymusic.apps -v 2 && \
-coverage report --skip-covered
+coveralls
