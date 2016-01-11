e82dea1fb31cadc341c6f4f479edd90a384e087d
Pando85 <pando855@gmail.com>
2016-01-11 02:24:36 +0100

    Added coveralls

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker-compose build web
 docker-compose -f test.yml build
-docker-compose -f test.yml run --rm web_test /usr/src/test.sh && \
-docker-compose -f test.yml stop
+docker-compose -f test.yml run --rm web_test /usr/src/test.sh
