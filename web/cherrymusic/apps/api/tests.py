8280112d887170c0133d03d483a7f1facd9544a5
Pando85 <pando855@gmail.com>
2016-01-13 20:52:30 +0100

    Added test api browse view

--- web/cherrymusic/apps/api/tests.py
+++ web/cherrymusic/apps/api/tests.py
@@ -368,7 +368,6 @@ class TestStatusView(APITestCase):
         self.user = User.objects.get(pk=1)
         self.client = APIClient(enforce_csrf_checks=True)
         self.client.force_authenticate(user=self.user)
-        self.serializer = TrackSerializer()
         self.url = reverse('api:status')
 
     def test_unauthenticated_status_query(self):
@@ -432,3 +431,42 @@ class TestGlobalSearchList(APITestCase):
         self.assertEqual(response.data, search_result_json)
 
 
+class TestBrowseView(APITestCase):
+    fixtures = ['directory', 'file', 'user']
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        self.client = APIClient(enforce_csrf_checks=True)
+        self.client.force_authenticate(user=self.user)
+        path = 'Paisano'
+        self.url = reverse('api:browse', args=[path])
+
+    def test_unauthenticated_browse_query(self):
+        client = APIClient()
+        response = client.get(self.url)
+
+        self.assertEqual(response.data, unauthenticated_response)
+
+    def test_browse_query(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        expected_data = {
+            "directories": [
+                {
+                    "parent": 11,
+                    "path": "Revolucion",
+                    "id": 12
+                }
+            ],
+            "files": [],
+            "current_path": "Paisano",
+            "current": {
+                "parent": 1,
+                "path": "Paisano",
+                "id": 11
+            },
+            "path": []
+        }
+        self.assertEqual(response.data, expected_data)
