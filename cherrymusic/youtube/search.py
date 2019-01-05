99e6a1fc25062c5e952482a21214d7ce6e63d126
Tom Wallroth <tomwallroth@gmail.com>
2019-01-06 00:51:02 +0100

    changed youtube search parser, convert duration to seconds int

--- cherrymusic/youtube/search.py
+++ cherrymusic/youtube/search.py
@@ -13,18 +13,26 @@ class YoutubeSearchResult:
     thumbnail_url: str
     title: str
     views: int
-    duration: str
+    duration: int
 
     def asdict(self):
         return dataclasses.asdict(self)
 
 
+def parse_duration_string(duration_string):
+    parts = list(map(int, duration_string.split(':')))
+    if len(parts) == 2:
+        return parts[0] * 60 + parts[1]
+    return parts[0] * 3600 + parts[1] * 60 + parts[2]
+
+
 def video_data_to_search_result(video_data):
+    duration_str = video_data['videoRenderer']['lengthText']['simpleText']
     return YoutubeSearchResult(
         youtube_id=video_data['videoRenderer']['videoId'],
         thumbnail_url=video_data['videoRenderer']['thumbnail']['thumbnails'][0]['url'],
         title=video_data['videoRenderer']['title']['simpleText'],
-        duration=video_data['videoRenderer']['lengthText']['simpleText'],
+        duration=parse_duration_string(duration_str),
         views=int(re.sub('\D', '', video_data['videoRenderer']['viewCountText']['simpleText'])),
     )
 
