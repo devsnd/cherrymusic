70de9af77f0a64f8011b44976702e32e6ef2f372
Tom Wallroth <tomwallroth@gmail.com>
2019-01-31 23:14:14 +0100

    implemented component that should be able to force keeping mobile screens on

---
+++ client/src/components/KeepScreenOn/KeepScreenOn.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <b-checkbox v-model="keepScreenOn">Keep Screen On</b-checkbox>
+        <!-- this image is just to get webpack to copy the video for me, something something fileloader -->
+        <img style="visibility: hidden" src="../../static/img/logo_video.png"></img>
+        <video class="hideVideo" src="../../static/client/logo_video.png" loop="loop" ref="loopVideo"></video>
+    </div>
+</template>
+<style scoped>
+    .hideVideo {
+        width: 1px;
+        height: 1px;
+        opacity: 0.01;
+        pointer-events: none;
+    }
+</style>
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'keepscreenon',
+        mounted: function () {
+          this.videoTag = this.$refs.loopVideo;
+        },
+        watch: {
+            keepScreenOn: function (keepItOn) {
+                if (keepItOn) {
+                    this.videoTag.play();
+                    console.log(this.videoTag);
+                } else {
+                    this.videoTag.pause();
+                }
+            }
+        },
+        data: function () {
+            return {
+                videoTag: null,
+                keepScreenOn: false,
+            }
+        }
+    });
+</script>
