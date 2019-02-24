dec751bb9029cde0b806f689d6eee8d0397adce5
Tom Wallroth <tomwallroth@gmail.com>
2019-02-24 16:46:00 +0100

    using swiping slides on mobile to switch between views

--- client/src/vue/MainView.vue
+++ client/src/vue/MainView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div style="position: fixed; z-index: 1; top: 0; width: 100%;">
+        <div style="position: fixed; z-index: 2; top: 0; width: 100%;">
             <cm-container style="padding: 0">
                 <cm-header></cm-header>
             </cm-container>
@@ -8,16 +8,36 @@
         <cm-container style="height: 100%;">
             <b-row style="height: 50px"><!-- space for the fixed navbar --></b-row>
             <b-row class="mt-3">
+
+            </b-row>
+            <template v-if="isMobile">
+                <swiper>
+                    <swiper-slide>
+                        <div v-if="viewMode === 'motd'">
+                            <MOTD></MOTD>
+                        </div>
+                        <div v-else-if="viewMode === 'browse'">
+                            <file-browser></file-browser>
+                        </div>
+                        <div v-else-if="viewMode === 'search'">
+                            <Scrollable>
+                                <search-results></search-results>
+                            </Scrollable>
+                        </div>
+                        <div v-else-if="viewMode === 'ytsearch'">
+                            <youtube-search></youtube-search>
+                        </div>
+                    </swiper-slide>
+                    <swiper-slide>
+                         <PlaylistManager></PlaylistManager>
+                    </swiper-slide>
+                </swiper>
+            </template>
+            <template v-else>
                 <b-col md="6">
                     <b-card>
                         <div v-if="viewMode === 'motd'">
-                            <b-jumbotron
-                                    header="Cherry Music II"
-                                    lead="Oh My Gaush, it's finally here" >
-                                <p>
-                                    Kapow!
-                                </p>
-                            </b-jumbotron>
+                            <MOTD></MOTD>
                         </div>
                         <div v-else-if="viewMode === 'browse'">
                             <file-browser></file-browser>
@@ -35,7 +55,7 @@
                 <b-col md="6">
                     <PlaylistManager></PlaylistManager>
                 </b-col>
-            </b-row>
+            </template>
             <Audioplayer></Audioplayer>
         </cm-container>
         <OptionsModal></OptionsModal>
@@ -55,11 +75,16 @@
     import SearchResults from '@/apps/search/components/SearchResults';
     import CmHeader from '@/components/Header/CmHeader';
     import FileBrowser from '@/apps/filebrowser/components/FileBrowser';
+    import MOTD from '@/apps/motd/components/MOTD';
     import YoutubeSearch from '@/apps/youtube/components/YoutubeSearch'
     import Vue from "vue";
     import Scrollable from '@/containers/Scrollable';
     import CmContainer from '@/components/layout/CmContainer';
     import OptionsModal from '@/components/options/OptionsModal';
+
+    //@ts-ignore
+    import { swiper, swiperSlide } from 'vue-awesome-swiper';
+    import 'swiper/dist/css/swiper.css'
 
     export default Vue.extend({
         name: 'dashboard',
@@ -78,10 +103,14 @@
             Scrollable,
             CmContainer,
             OptionsModal,
+            MOTD,
+            swiper,
+            swiperSlide,
         },
         computed: {
             ...mapGetters({
-                viewMode: 'mainview/viewMode'
+                viewMode: 'mainview/viewMode',
+                isMobile: 'mobilemode/isMobile',
             })
         },
         methods: {
